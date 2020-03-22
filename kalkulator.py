7187d89b29ddcb174921c1562d427fb9ea61941b
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-03-22 13:02:35 +0100

    Empty window

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,17 @@
 from PyQt5.QtWidgets import QApplication, QWidget
-print("hello")
+class Kalkulator(QWidget):
+    def __init__(self, parent=None):
+         super().__init__(parent)
+         self.interfejs()
+
+    def interfejs(self):
+        self.resize(300,100)
+        self.setWindowTitle("Prosty kalkulator")
+        self.show()
+
+if __name__ == '__main__':
+    import sys
+
+    app=QApplication(sys.argv)
+    okno = Kalkulator()
+    sys.exit(app.exec_())
